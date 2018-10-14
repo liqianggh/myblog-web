af71ab03fb4cdc3bbf5cdc1cd363a50d56fda69e
Jann Lee <liqiang1chn@163.com>
2018-10-14 13:01:16 +0800

    头部导航栏

---
+++ src/pages/index/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import HeaderMenus from 'common/HeaderMenus'
+export default {
+  name: 'Index',
+  components: {
+    HeaderMenus
+  },
+  data () {
+    return {
+      msg: 'Welcome!'
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+  .el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  }
+
+  .el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+  }
+
+  .el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  }
+</style>
