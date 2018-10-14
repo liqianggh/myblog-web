1f9d0a9ea7773a13b21480a3654f826c4af9f57d
Jann Lee <liqiang1chn@163.com>
2018-10-14 11:21:46 +0800

    引入elementUi

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 整合element-ui
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // 初始化css
 import 'styles/reset.css'
 // 1像素边框解决，兼容多倍屏幕
 import 'styles/border.css'
 
 Vue.config.productionTip = false
-
+// todo 按需引入element-ui中的组件 以达到项目瘦身
+Vue.use(Element)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })
