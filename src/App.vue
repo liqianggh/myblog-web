8e16f88eacc12fcd7f728872728818957a0baadf
liqianggh <liqiang1chn@163.com>
2019-07-13 22:47:52 +0800

    Enhancement: cnzz统计

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,24 @@ export default {
   components: {
     HeaderMenus,
     CopyRight
+  },
+  mounted() {
+    // 友盟统计添加
+    const script = document.createElement("script");
+    script.src =
+      "https://s23.cnzz.com/z_stat.php?id=1277647101&web_id=1277647101";
+    script.language = "JavaScript";
+    document.body.appendChild(script);
+  },
+  watch: {
+    $route() {
+      if (window._czc) {
+        let location = window.location;
+        let contentUrl = location.pathname + location.hash;
+        let refererUrl = "/";
+        window._czc.push(["_trackPageview", contentUrl, refererUrl]);
+      }
+    }
   }
 }
 </script>
