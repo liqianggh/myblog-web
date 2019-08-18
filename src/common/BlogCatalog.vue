0b63bf2cbc0c19ff78e0048bad948f7664684fae
liqianggh <liqiang1chn@163.com>
2019-08-18 12:58:42 +0800

    Enhancement: 文章目录

---
+++ src/common/BlogCatalog.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside class="l_box blog-left">
+    <div class="article-catalog fenlei">
+      <h2 >文章目录</h2>
+      <div class="catalog-list">
+        <a
+          class="list-item"
+          :data-lev="item.lev"
+          v-for="(item, index) in this.catalogs"
+          :href="'#'+item.id"
+          :key="index"
+        >
+          {{item.text}}
+        </a>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'BlogCatalog',
+  props: {
+    catalogs: []
+  },
+  mounted(){
+
+  }
+}
+</script>
--- src/pages/detail/Article.vue
+++ src/pages/detail/Article.vue
@@ -2,8 +2,8 @@
   <div>
     <header-menus></header-menus>
     <article>
-      <left-side></left-side>
-      <main>
+      <blog-catalog :catalogs="articleData.catalogs"></blog-catalog>
+      <main class="blog_detail">
         <article-info :id="id" :articleData="articleData"></article-info>
         <comment-list :targetId="id"  :commentsPageInfoData ="commentsPageInfo"  :targetType="target_type"></comment-list>
       </main>
@@ -18,9 +18,12 @@ import ArticleInfo from './ArticleInfo'
 import CommentList from 'common/CommentList'
 import axios from 'Axios'
 import LeftSide from 'common/LeftSide'
+import BlogCatalog from "../../common/BlogCatalog";
 export default {
   name: 'Comment',
   components: {
+    BlogCatalog,
+    BlogCatalog,
     LeftSide,
     HeaderMenus,
     CopyRight,
@@ -82,6 +85,31 @@ export default {
 }
 </script>
 <style lang='stylus'>
+  .article-catalog  h2 {
+    color: #e04747;
+    font-size: 14px;
+    line-height: 30px;
+    padding-left: 20px;
+    background: #fff;
+    border-bottom: 1.2px solid #eee;
+  }
+  .l_box div {
+    background: rgba(255,255,255,0.5);
+    margin-bottom: 0px;
+    overflow: hidden;
+  }
+  .blog_detail{
+    width: 79%
+  }
+  @media only screen and (max-width: 479px){
+    .blog_detail{
+      width: 100%;
+    }
+  }
+  .blog-left{
+    width 20%
+    background-color none
+  }
   .news_con markdown-body pre, .markdown-body pre {
     padding: 0px;
     overflow: auto;
@@ -91,4 +119,51 @@ export default {
     border-radius: 3px;
     border: dashed 0.5px #9e9e9e;
   }
+
+ .article-catalog {
+    /*position:fixed;*/
+    /*top:1.071429rem;*/
+    /*width:100%*/
+   width: 11%
+   position: fixed;
+   top: 5.071429rem;
+  }
+
+ .article-catalog .catalog-title {
+    margin-bottom:.178571rem;
+    color:#999;
+    font-size:.357143rem;
+    font-weight:700
+  }
+ .article-catalog .list-item {
+    display:block;
+    line-height:2;
+    overflow:hidden;
+    white-space:nowrap;
+    text-overflow:ellipsis;
+    color:#204040
+  }
+ .article-catalog .list-item:hover {
+    background-color:#f56c6c47
+  }
+  .article-catalog .list-item[data-lev="1"] {
+    padding-left:1.071429rem;
+  }
+ .article-catalog .list-item[data-lev="2"] {
+    padding-left:2.071429rem;
+    font-weight:700
+  }
+  .article-catalog .list-item[data-lev="3"] {
+    padding-left:3.178571rem;
+    font-weight:500
+  }
+ .article-catalog .list-item[data-lev="4"] {
+   padding-left:3.178571rem;
+  }
+ .article-catalog .list-item[data-lev="5"] {
+   padding-left:3.178571rem;
+  }
+
+
+
 </style>
